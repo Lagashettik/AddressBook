a7eff974567565659b5f54bd8408a2043a995657
Lagashettik <Lagashettik@gmail.com>
2021-02-11 16:56:43 +0530

    Welcome Message

---
+++ src/AddressBook.java
@@ -0,0 +1,10 @@
+public class AddressBook {
+
+    AddressBook(){
+        System.out.println("Welcome to Address Book Program");
+    }
+
+    public static void main(String[] args) {
+        new AddressBook();
+    }
+}
